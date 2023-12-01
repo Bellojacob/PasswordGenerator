346b93811b1c866f17e4eac2cd8113dbd42a8716
Bellojacob <jacobbello2002@gmail.com>
2023-11-30 22:27:01 -0800

    Add password generator GUI

--- Main.java
+++ Main.java
@@ -1,44 +1,127 @@
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
 public class Main {
-    public static void main(String[] args) {
+    static Random random;
+    static StringBuilder password;
+    static String symbols;
+    static String ints;
+    static String lowercaseChars;
+    static String uppercaseChars;
+    static String wordHomie;
+
+    public static void welcome() {
         System.out.println("Welcome To Password Generator 1.0");
         System.out.println("Generating Password...");
+    }
+
+    public static void variables() {
         Random random = new Random();
+        password = new StringBuilder();
+        symbols = "!@#$%^&*_-+=;:'<>/?";
+        ints = "1234567890";
+        lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
 
-        StringBuilder password = new StringBuilder();
-        String symbols = "!@#$%^&*_-+=;:'<>/?";
-        String ints = "1234567890";
-        String lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-        String uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+    public static void loop() {
+        Random random = new Random();
         for (int i = 0; i < 12; i++) {
             int numberRandom = random.nextInt(1000);
-            // System.out.println(numberRandom);
             if (numberRandom < 250) {
-                // Add a random symbol to the password
                 int symbolIndex = random.nextInt(symbols.length());
                 char randoSymbo = symbols.charAt(symbolIndex);
                 password.append(randoSymbo);
-
-            } else if (numberRandom < 500 ) {
-                // Add a random integer to the password
+            } else if (numberRandom < 500) {
                 int intIndex = random.nextInt(ints.length());
                 char randoInt = ints.charAt(intIndex);
                 password.append(randoInt);
             } else if (numberRandom < 750) {
-                // Add a random lowercase character to the password
                 int lowerIndex = random.nextInt(lowercaseChars.length());
                 char randoLower = lowercaseChars.charAt(lowerIndex);
                 password.append(randoLower);
-            } else if (numberRandom >= 750)  {
-                // Add a random uppercase character to the password
+            } else if (numberRandom >= 750) {
                 int upperIndex = random.nextInt(uppercaseChars.length());
                 char randoUpper = uppercaseChars.charAt(upperIndex);
                 password.append(randoUpper);
             }
         }
-
-        System.out.println("Generated Password: " + password.toString());
     }
+
+    public static void box() {
+        JFrame frame = new JFrame("Password Generator");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JPanel mainPanel = new JPanel(new GridLayout(5, 1));
+
+        JPanel topPanel = new JPanel(new FlowLayout());
+        JLabel label1 = new JLabel("Welcome to Password Generator", SwingConstants.CENTER);
+        label1.setFont(new Font("Serif", Font.PLAIN, 24));
+        topPanel.add(label1);
+        mainPanel.add(topPanel);
+
+        JPanel secondPanel = new JPanel(new FlowLayout());
+        JLabel label2 = new JLabel("Password Length?", SwingConstants.CENTER);
+        label2.setFont(new Font("Serif", Font.PLAIN, 20));
+        secondPanel.add(label2);
+        mainPanel.add(secondPanel);
+
+        JPanel sliderPanel = new JPanel(new FlowLayout());
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 6, 18, 6);
+        slider.setMajorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setPreferredSize(new Dimension(400, 50)); // Adjust the width and height as needed
+        slider.addChangeListener(new javax.swing.event.ChangeListener() {
+            @Override
+            public void stateChanged(javax.swing.event.ChangeEvent e) {
+                int passwordLength = slider.getValue();
+                // Do something with the selected password length
+            }
+        });
+            sliderPanel.add(slider);
+            mainPanel.add(sliderPanel);
+
+            JButton button = new JButton("Generate Password");
+            button.addActionListener(e -> generatePassword());
+            mainPanel.add(button);
+
+            JTextField passwordField = new JTextField(20);
+            passwordField.setEditable(false);
+            mainPanel.add(passwordField);
+
+            frame.add(mainPanel);
+            frame.setLayout(new FlowLayout()); // Add this line to set the layout
+            frame.setVisible(true);
+        }
+
+        public static void generatePassword() {
+            // Implement your password generation logic here
+            String wordHomie = "password";
+        }
+    
+
+    public static void main(String[] args) {
+        welcome();
+        variables();
+        loop();
+        System.out.println("Generated Password: " + password.toString());
+        box();
+    }
+
 }
